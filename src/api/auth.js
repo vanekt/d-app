ce180eae5dd36d03bbb2d326c63880c6bc01067a
Ivan Tkachenko <vanekt@yandex.ru>
2018-07-28 23:32:28 +0300

    esdoc

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,22 +1,40 @@
 import { apiVersion, getOptions, postOptions, toJson } from './common';
 
+/**
+ * Auth api.
+ */
 export default class {
+  /**
+   * @param {string} apiUrl - адрес api-сервера
+   * @param {string} version - версия api, например v1
+   */
   constructor(apiUrl, version = apiVersion) {
+    /**
+     * @type {string}
+     */
     this.url = apiUrl + '/api/' + version;
   }
 
-  login(data) {
+  /**
+   * User login method
+   * @param {string} username - имя пользователя
+   * @param {string} password - пароль
+   */
+  login(username, password) {
     return this.fetch(this.url + '/auth/login', {
       ...postOptions,
       body: JSON.stringify({
-        login: data.username,
-        password: data.password
+        username: username,
+        password: password
       })
     })
       .then(toJson)
       .catch(errorToTransport);
   }
 
+  /**
+   * User logout method
+   */
   logout() {
     return this.fetch(this.url + '/auth/logout', getOptions)
       .then(toJson)
